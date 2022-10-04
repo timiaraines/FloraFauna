e8c581e2b63712d9a5777dbf0dd4edecb2548a86
timiaraines <72035201+timiaraines@users.noreply.github.com>
2022-10-03 23:25:12 -0500

    Initial Commit

---
+++ FloraFauna/FloraFaunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloraFaunaApp.swift
+//  FloraFauna
+//
+//  Created by Timia Raines on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FloraFaunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
